de86870894678e87db1705de747c94a4979b4ea2
PranayK27 <kekre.pranay@gmail.com>
2022-09-17 19:40:16 +0100

    submodules

---
+++ email-service/src/test/java/com/pranay/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranay.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ order-service/src/main/java/com/pranay/orderservice/controller/OrderController.java
@@ -0,0 +1,38 @@
+package com.pranay.orderservice.controller;
+
+import com.pranay.basedomains.dto.Order;
+import com.pranay.basedomains.dto.OrderEvent;
+import com.pranay.orderservice.kafka.OrderProducer;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1")
+public class OrderController {
+
+    private OrderProducer orderProducer;
+
+    public OrderController(OrderProducer orderProducer) {
+        this.orderProducer = orderProducer;
+    }
+
+    @PostMapping("/orders")
+    public String placeOrder(@RequestBody Order order){
+
+        order.setOrderId(UUID.randomUUID().toString());
+
+        OrderEvent orderEvent = new OrderEvent();
+        orderEvent.setStatus("PENDING");
+        orderEvent.setMessage("order status is in pending state");
+        orderEvent.setOrder(order);
+
+        orderProducer.sendMessage(orderEvent);
+
+        return "Order placed successfully ...";
+    }
+}
+
